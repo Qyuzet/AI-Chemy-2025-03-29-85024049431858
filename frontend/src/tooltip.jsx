fca0098ceb41877fc8440742e90ed9b4e27c8f2c
ICP.Ninja <noreply@icp.ninja>
2025-03-29 15:50:41 +0700

    ICP.Ninja export

---
+++ frontend/src/tooltip.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const Tooltip = ({ children, content }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div className="relative inline-block">
+      <div 
+        className="flex items-center cursor-help"
+        onMouseEnter={() => setIsVisible(true)}
+        onMouseLeave={() => setIsVisible(false)}
+      >
+        {children}
+        <span className="ml-1 text-zinc-400 hover:text-zinc-300">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+            <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm11.378-3.917c-.89-.777-2.366-.777-3.255 0a.75.75 0 01-.988-1.129c1.454-1.272 3.776-1.272 5.23 0 1.513 1.324 1.513 3.518 0 4.842a3.75 3.75 0 01-.837.552c-.676.328-1.028.774-1.028 1.152v.75a.75.75 0 01-1.5 0v-.75c0-1.279 1.06-2.107 1.875-2.502.182-.088.351-.199.503-.331.83-.727.83-1.857 0-2.584zM12 18a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
+          </svg>
+        </span>
+      </div>
+      {isVisible && (
+        <div className="absolute z-50 w-64 p-2 mt-2 text-sm text-zinc-200 bg-zinc-800 rounded-lg shadow-lg border border-zinc-700">
+          {content}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Tooltip;
